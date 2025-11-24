f9e8d3e9eb9c3d5f437e2469472243ec34e2e3cd
Leah-saad88 <leasaad2022@icloud.com>
2025-11-24 14:54:26 +0200

    i switched to the way you did it in class it better

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,111 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class Book{
-  final String title;
-  final String category;
-  final String description;
-  final String image; // we are doing this  is for the asset path
-  final double rating;
 
+class ListViewDemo extends StatefulWidget {
+  const ListViewDemo({super.key});
 
-Book({
-  required this.title,
-  required this.category,
-  required this.description,
-  required this.image,
-  required this.rating,
+  @override
+  State<ListViewDemo> createState() => _ListViewDemoState();
+}
+class _ListViewDemoState extends State<ListViewDemo> {
+  final List<Map<String, String>> data = [
+    {
+      "Title": "Fake skating",
+      "category": "Teen Romance",
+      "description": "11",
+      "image": "assets/fk.jpg",
+      "rating": 4.5,
+    },
+    {
+      "Title":"Better than the movies",
+      "category": "Romance/comedy",
+      "description": "11",
+      "image": "assets/mv.webp",
+      "rating": 4.5,
+    },
+      {
+      "Title":"Starry eyes",
+      "category": "Romance/comedy",
+      "description": "11",
+      "image": "assets/st.jfif",
+      "rating": 4.2,
+    },
 
-});
+     {
+      "Title":"Five survive",
+      "category": "Thriller",
+      "description": "11",
+      "image": "assets/fs.jfif",
+      "rating": 4.7,
+    },
+     {
+      "Title":"Good girl's guide to murder",
+      "category": "thriller",
+      "description": "11",
+      "image": "assets/fd.jpg" ,
+      "rating": 4.7,
+    },
+     {
+      "Title":"one of us is lying",
+      "category": "thriller",
+      "description": "11",
+      "image": "assets/on.jfif",
+      "rating": 4.8,
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ListView Demo'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              SizedBox(height: 10),
+              SizedBox(
+                width: 400,
+                height: 80,
+                child: Card(
+                  color: Colors.brown,
+                  child: ListTile(
+                    title: Text(
+                      data[index]["Title"]!,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      data[index]["SubTitle"]!,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    leading: Image.network(data[index]["Image"]!, width: 40),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => NewPage(
+                            title: data[index]["Title"]!,
+                            subtitle: data[index]["SubTitle"]!,
+                            image: data[index]["Image"]!,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
 }
 
- final list<Book> books =[
-Book(
- title: "Fake skating",
- category: "Teen Romance",
- description:"11",
- image:"",
- rating:4.5,
-),
-Book(
- title: "Better than the movies",
- category: "Romance/comedy",
- description:"11",
- image:"",
- rating:4.5,
-),
-Book(
- title: "The do over",
- category: "Romance/comedy",
- description:"11",
- image:"",
- rating:4.2,
-),
-Book(
- title: "Five survive",
- category: "Thriller",
- description:"11",
- image:"",
- rating:4.8,
-),
-
-Book(
- title: "good girl's guide to murder",
- category: "Thriller ",
- description:"11",
- image:"",
- rating:4.5,
-),
-Book(
- title: "One of us is lying",
- category: "Thriller/horror ",
- description:"11",
- image:"",
- rating:4.7,
-),
-Book(
- title: "One of us is lying",
- category: "Thriller/horror ",
- description:"11",
- image:"",
- rating:4.7,
-),
-Book(
- title: "Harry potter",
- category: "Thriller/Fantasy ",
- description:"11",
- image:"",
- rating:4.7,
-),
-Book(
- title: "The cruel prince ",
- category: "Thriller/Fantasy ",
- description:"11",
- image:"",
- rating:4.5,
-),
-Book(
- title: "Lightlark",
- category: "Thriller/Fantasy",
- description:"11",
- image:"",
- rating:4.7,
-),
-
- ]
 
 void main() {
   runApp(const MyApp());
@@ -121,19 +134,28 @@ class MyApp extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-
-          child: Padding(
-            padding: const EdgeInsets.only(top: 79),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Text(
-                "welcome to our Book club",
-                style: const TextStyle(
-                  fontSize: 24,
-                  color: Color.fromARGB(179, 12, 10, 10),
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 79),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Text(
+                      "welcome to our Book club",
+                      style: const TextStyle(
+                        fontSize: 24,
+                        color: Color.fromARGB(179, 12, 10, 10),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+
+                const SizedBox(height: 30),
+
+                
+              ],
             ),
           ),
         ),
